eb20e6a7f35606135b789e9e29883d33b38f98eb
Javier Aceña <40375118+j0nl1@users.noreply.github.com>
2021-08-07 20:13:54 +0200

    features: add value objects and aggregate root (#1)
    * add: string value object
    
    * add: number value object
    
    * add: event interface
    
    * add: aggregate root

---
+++ src/modules/shared/domain/aggregate-root.ts
@@ -0,0 +1,17 @@
+import Event from './event.interface';
+
+const INTERNAL_EVENTS = Symbol();
+
+export abstract class AggregateRoot {
+  private readonly [INTERNAL_EVENTS]: Event[] = [];
+
+  public pullDomainEvents(): Event[] {
+    const events = [...this[INTERNAL_EVENTS]];
+    this[INTERNAL_EVENTS].length = 0;
+    return events;
+  }
+
+  public record(event: Event): void {
+    this[INTERNAL_EVENTS].push(event);
+  }
+}
---
+++ src/modules/shared/domain/number.vo.ts
@@ -0,0 +1,6 @@
+export default abstract class NumberValueObject {
+  readonly value;
+  constructor(value: number) {
+    this.value = value;
+  }
+}
